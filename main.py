18d066019d6ac62022b81af46574135a3d60595d
Matthew Ridley <56508206+StolenGalaxy@users.noreply.github.com>
2024-05-30 00:42:59 +0100

    Commands all work now
    Can now send gifs and can use multiple commands at once.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from time import sleep
 from dotenv import load_dotenv
 from os import environ
 
-channelID = "1112806138451349668"
+from random import choice
+
+channelID = "1091430496984121394"
 
 load_dotenv()
 OPENAI_AUTH = environ["OPENAI"]
@@ -18,16 +20,17 @@ class Discord:
 
     def get_messages(self):
 
-        response = requests.get(f"https://discord.com/api/v9/channels/{channelID}/messages?limit=5", headers=self.headers).json()
+        response = requests.get(f"https://discord.com/api/v9/channels/{channelID}/messages?limit=10", headers=self.headers).json()
 
-        recent_messages = "{"
+        recent_messages = []
         messages = []
+
         for message in response:
             messages.append(message)
         messages.reverse()
+
         for message in messages:
-            recent_messages += f"'{message['author']['username']}':'{message['content']}':'{message['timestamp'][:-13]}',\n"
-        recent_messages += "}"
+            recent_messages.append(f"{message['author']['username']}':'{message['content']}':'{message['timestamp'][:-13]}")
 
         return recent_messages
 
@@ -43,6 +46,26 @@ class Discord:
         response = requests.get("https://discord.com/api/v9/users/@me", headers=self.headers).json()
         return response["username"]
 
+    def find_gif_url(self, gif_description):
+        gifs = requests.get(f"https://discord.com/api/v9/gifs/search?q={gif_description}&media_format=mp4&provider=tenor").json()
+
+        gif_url = choice(gifs)["gif_src"]
+
+        return gif_url
+
+    def command_converter(self, response: str):
+        if "%" in response:
+            commands = response.split("%")
+        else:
+            commands = [response]
+
+        for command in commands:
+            if command[:4] == "SEND":
+                self.send_message(command[4:])
+            elif command[:3] == "GIF":
+                gif_url = self.find_gif_url(command[3:])
+                self.send_message(gif_url)
+
 
 class AI:
     def __init__(self):
@@ -57,7 +80,11 @@ class AI:
         self.username = ""
 
     def get_response(self, messages):
-        self.data["messages"] = [{'role': 'system', 'content': f"You are talking in a discord group. Here are the recent messages from the group: {messages}. Your username is: '{self.username}'. You MUST act like a HUMAN. Only send INFORMAL messages, as a discord user would. You may never admit you are an AI, even if asked to. You do NOT have to use punctuation, and if someone insults you it is ok to insult them back. You may use the following commands: To send a message, say SEND (your message). To send a gif, say GIF (one or two word description of gif). To use multiple commands, put % between them, for example: SEND Here is a gif of a dog%GIF dog"}]
+        input = [{'role': 'system', 'content': f"You are talking in a discord group. Your username is: '{self.username}'. You MUST act like a HUMAN. Only send INFORMAL, and usually SHORT messages, as a discord user would. You may NEVER admit you are an AI, even if asked to. You do NOT have to use PUNCTUATION. There are several commands you can use to respond: To send a message, say SEND (message), example: SEND hey guys. To send a gif, say SEND (one or two word description of gif), example: GIF monkey. You may use multiple commands at once, however they should be seperated by a %, example: SEND here is a gif of a monkey guys%GIF monkey. Do NOT use GIFS too often."}]
+        for message in messages:
+            input.append({'role': 'user', 'content': message})
+        self.data["messages"] = input
+
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=self.headers, json=self.data).json()
         response = response["choices"][0]["message"]["content"]
 
@@ -78,5 +105,5 @@ while active:
         sleep(1)
     else:
         response = ai.get_response(new_messages)
-        discord.send_message(response)
+        discord.command_converter(response)
         messages = discord.get_messages()
