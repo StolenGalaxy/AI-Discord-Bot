b92c5f0e032ce2f724ec0991c9f20b4c6a1d5203
Matthew Ridley <56508206+StolenGalaxy@users.noreply.github.com>
2025-08-04 17:49:26 +0100

    Began implementing response format

--- new_main.py
+++ new_main.py
@@ -3,20 +3,30 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from os import environ
 
+from pydantic import BaseModel
+
 load_dotenv()
 
-OPENAI_AUTH = environ["OPENAI"]
 DISCORD_AUTH = environ["DISCORD"]
 
 SYSTEM_PROMPT = """
 
+You will be shown messages from a Discord group/server.
+
+You must respond to these messages as if you are a human.
+
+Your responses should be informal and short unless told otherwise.
 
 """
 
 
-class client(OpenAI):
-    def __init__(self, *, api_key=OPENAI_AUTH):
-        super().__init__(api_key=api_key)
+class ResponseFormat(BaseModel):
+    messages_to_send: list[str]
+
+
+class Client(OpenAI):
+    def __init__(self):
+        super().__init__()
 
     def get_prompt(self):
         prompt = SYSTEM_PROMPT
@@ -24,16 +34,23 @@ class client(OpenAI):
         return prompt
 
     def get_response(self):
-        completion = self.chat.completions.create(
+        completion = self.chat.completions.parse(
             model="gpt-4.1",
             messages=[
                 {
                     "role": "system",
                     "content": self.get_prompt()
                 }
-            ]
+            ],
+            response_format=ResponseFormat,
         )
 
         response = completion.choices[0].message.content
 
         return response
+
+
+my_client = Client()
+
+response = my_client.get_response()
+print(response)
